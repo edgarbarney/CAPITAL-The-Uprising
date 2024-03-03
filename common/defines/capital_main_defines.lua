47c245de87e224258301875cbfef04ea8157a848
FranticDreamer <edwardgmgmd@gmail.com>
2024-03-03 15:18:46 +0300

    I Promise I will use proper commit messages after this

--- common/defines/capital_main_defines.lua
+++ common/defines/capital_main_defines.lua
@@ -1,33 +1,34 @@
-NDefines.NGame.START_DATE = "1936.1.1.12"
-NDefines.NGame.END_DATE = "1970.1.1.1"
+NDefines.NGame.START_DATE = "2022.1.1.12"
+NDefines.NGame.END_DATE = "2100.1.1.1"
 
 NDefines.NFrontend.CAMERA_MIN_HEIGHT = 50.0
 NDefines.NFrontend.CAMERA_MAX_HEIGHT = 3800.0
 NDefines.NGraphics.PROVINCE_NAME_DRAW_DISTANCE = 1000.0 			-- Remove province names beyond this distance
 
-NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 1.74
-NDefines.NGame.SAVE_VERSION = 7
+NDefines.NGame.MAP_SCALE_PIXEL_TO_KM = 7.114 -- Originally, 7.114. They did the math, But it looks too weird.
+--NDefines.NGame.SAVE_VERSION = 7
 
-NDefines.NSupply.CAPITAL_SUPPLY_BASE = 1.6 -- base supply for capital
-NDefines.NSupply.CAPITAL_INITIAL_SUPPLY_FLOW = 3.0 -- starting supply from capital
-NDefines.NSupply.CAPITAL_STARTING_PENALTY_PER_PROVINCE = 0.3 -- starting penalty that will be added as supply moves away from its origin (modified by stuff like terrain)
-NDefines.NSupply.CAPITAL_ADDED_PENALTY_PER_PROVINCE = 0.4 -- added penalty as we move away from origin
-NDefines.NSupply.NODE_INITIAL_SUPPLY_FLOW = 2.0
-NDefines.NSupply.NODE_STARTING_PENALTY_PER_PROVINCE = 0.3
-NDefines.NSupply.NODE_ADDED_PENALTY_PER_PROVINCE = 0.4
-NDefines.NSupply.INFRA_TO_SUPPLY = 0.15							-- each level of infra gives this many supply
-NDefines.NSupply.VP_TO_SUPPLY_BONUS_CONVERSION = 0.02			-- Bonus to supply local supplies from Victory Points, multiplied by this aspect and rounded to closest integer
-NDefines.NSupply.VP_TO_SUPPLY_BASE = 0							-- Bonus to supply from a VP, no matter the level
-NDefines.NSupply.NAVAL_BASE_INITIAL_SUPPLY_FLOW = 0.8
-NDefines.NSupply.NAVAL_BASE_STARTING_PENALTY_PER_PROVINCE = 0.6
-NDefines.NSupply.NAVAL_BASE_ADDED_PENALTY_PER_PROVINCE = 0.6
-NDefines.NSupply.FLOATING_HARBOR_INITIAL_SUPPLY_FLOW = 0.8
+--NDefines.NSupply.CAPITAL_SUPPLY_BASE = 1.6 -- base supply for capital
+--NDefines.NSupply.CAPITAL_INITIAL_SUPPLY_FLOW = 3.0 -- starting supply from capital
+--NDefines.NSupply.CAPITAL_STARTING_PENALTY_PER_PROVINCE = 0.3 -- starting penalty that will be added as supply moves away from its origin (modified by stuff like terrain)
+--NDefines.NSupply.CAPITAL_ADDED_PENALTY_PER_PROVINCE = 0.4 -- added penalty as we move away from origin
+--NDefines.NSupply.NODE_INITIAL_SUPPLY_FLOW = 2.0
+--NDefines.NSupply.NODE_STARTING_PENALTY_PER_PROVINCE = 0.3
+--NDefines.NSupply.NODE_ADDED_PENALTY_PER_PROVINCE = 0.4
+--NDefines.NSupply.INFRA_TO_SUPPLY = 0.15							-- each level of infra gives this many supply
+--NDefines.NSupply.VP_TO_SUPPLY_BONUS_CONVERSION = 0.02			-- Bonus to supply local supplies from Victory Points, multiplied by this aspect and rounded to closest integer
+--NDefines.NSupply.VP_TO_SUPPLY_BASE = 0							-- Bonus to supply from a VP, no matter the level
+--NDefines.NSupply.NAVAL_BASE_INITIAL_SUPPLY_FLOW = 0.8
+--NDefines.NSupply.NAVAL_BASE_STARTING_PENALTY_PER_PROVINCE = 0.6
+--NDefines.NSupply.NAVAL_BASE_ADDED_PENALTY_PER_PROVINCE = 0.6
+--NDefines.NSupply.FLOATING_HARBOR_INITIAL_SUPPLY_FLOW = 0.8
 
-NDefines.NAir.AIR_REGION_SUPERIORITY_PIXEL_SCALE = 0.02
+--NDefines.NAir.AIR_REGION_SUPERIORITY_PIXEL_SCALE = 0.02
 
 --NDefines.NNavy.NAVAL_SPEED_MODIFIER = 0.3
 --NDefines.NNavy.NAVAL_TRANSFER_BASE_SPEED = 6
-NDefines.NNavy.NAVAL_RANGE_TO_INGAME_DISTANCE = 0.24							-- Scale the ship stats "naval_range" to the ingame distance
+
+--NDefines.NNavy.NAVAL_RANGE_TO_INGAME_DISTANCE = 0.24							-- Scale the ship stats "naval_range" to the ingame distance
 
 NDefines.NGraphics.MINIMUM_PROVINCE_SIZE_IN_PIXELS = 4
 
@@ -35,6 +36,10 @@ NDefines.NGraphics.COUNTRY_FLAG_LARGE_STRIPE_MAX_HEIGHT = 24000        -- VANILL
 NDefines.NGraphics.COUNTRY_FLAG_SMALL_TEX_MAX_SIZE = 512            -- VANILLA: 64
 NDefines.NGraphics.COUNTRY_FLAG_STRIPE_TEX_MAX_HEIGHT = 8196        -- VANILLA: 2048
 NDefines.NGraphics.COUNTRY_FLAG_TEX_MAX_SIZE = 2048                    -- VANILLA: 256
+
+NDefines.NGraphics.COUNTRY_FLAG_TEX_WIDTH = 164
+NDefines.NGraphics.COUNTRY_FLAG_TEX_HEIGHT = 104
+
 NDefines.NGraphics.VICTORY_POINT_MAP_ICON_TEXT_CUTOFF = {225, 375, 750}  -- At what camera distance the VP name text disappears.
 NDefines.NGraphics.VICTORY_POINTS_DISTANCE_CUTOFF = {375,750,1500} -- At what distance VPs are hidden
 NDefines.NGraphics.CAPITAL_ICON_CUTOFF = 1650	-- At what camera distance capital icons disappears
@@ -50,20 +55,20 @@ NDefines.NGame.AIR_LOG_TIMEOUT_HOURS = 1 ---Made By Deha#4328
 NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 60 ---Made By Deha#4328
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 120 ---Made By Deha#4328
 
-NDefines.NMilitary.LAND_SPEED_MODIFIER = 0.3 ---Made By Deha#4328
-NDefines.NMilitary.COMBAT_MOVEMENT_SPEED = 0.99 ---Made By Deha#4328
-NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5 ---Made By Deha#4328
-NDefines.NMilitary.RESISTANCE_GROWTH_BASE = 0.0 ---Made By Deha#4328
-NDefines.NMilitary.RESISTANCE_GROWTH_MIN = 0.0 ---Made By Deha#4328
-NDefines.NMilitary.COMPLIANCE_GROWTH_BASE = 2.5 ---Made By Deha#4328
-NDefines.NMilitary.COMPLIANCE_GROWTH_MIN = 0.0 ---Made By Deha#4328
-
-NDefines.NProduction.BASE_FACTORY_SPEED = 35 ---Made By Deha#4328
-NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 20.0 ---Made By Deha#4328
-NDefines.NProduction.BASE_FACTORY_SPEED_NAV = 10.0 ---Made By Deha#4328
-NDefines.NProduction.BASE_FACTORY_START_EFFICIENCY_FACTOR = 50 ---Made By Deha#4328
-NDefines.NProduction.BASE_FACTORY_MAX_EFFICIENCY_FACTOR = 500 ---Made By Deha#4328
-NDefines.NProduction.BASE_FACTORY_EFFICIENCY_GAIN = 5 ---Made By Deha#4328
-NDefines.NProduction.MAX_CIV_FACTORIES_PER_LINE = 20 ---Made By Deha#4328
-NDefines.NProduction.PRODUCTION_RESOURCE_LACK_PENALTY = 0.0 ---Made By Deha#4328
-NDefines.NProduction.MAX_MIL_FACTORIES_PER_LINE = 300 ---Made By Deha#4328
+-- NDefines.NMilitary.LAND_SPEED_MODIFIER = 0.3 ---Made By Deha#4328
+-- NDefines.NMilitary.COMBAT_MOVEMENT_SPEED = 0.99 ---Made By Deha#4328
+-- NDefines.NMilitary.DEPLOY_TRAINING_MAX_LEVEL = 5 ---Made By Deha#4328
+-- NDefines.NMilitary.RESISTANCE_GROWTH_BASE = 0.0 ---Made By Deha#4328
+-- NDefines.NMilitary.RESISTANCE_GROWTH_MIN = 0.0 ---Made By Deha#4328
+-- NDefines.NMilitary.COMPLIANCE_GROWTH_BASE = 2.5 ---Made By Deha#4328
+-- NDefines.NMilitary.COMPLIANCE_GROWTH_MIN = 0.0 ---Made By Deha#4328
+ 
+-- NDefines.NProduction.BASE_FACTORY_SPEED = 35 ---Made By Deha#4328
+-- NDefines.NProduction.BASE_FACTORY_SPEED_MIL = 20.0 ---Made By Deha#4328
+-- NDefines.NProduction.BASE_FACTORY_SPEED_NAV = 10.0 ---Made By Deha#4328
+-- NDefines.NProduction.BASE_FACTORY_START_EFFICIENCY_FACTOR = 50 ---Made By Deha#4328
+-- NDefines.NProduction.BASE_FACTORY_MAX_EFFICIENCY_FACTOR = 500 ---Made By Deha#4328
+-- NDefines.NProduction.BASE_FACTORY_EFFICIENCY_GAIN = 5 ---Made By Deha#4328
+-- NDefines.NProduction.MAX_CIV_FACTORIES_PER_LINE = 20 ---Made By Deha#4328
+-- NDefines.NProduction.PRODUCTION_RESOURCE_LACK_PENALTY = 0.0 ---Made By Deha#4328
+-- NDefines.NProduction.MAX_MIL_FACTORIES_PER_LINE = 300 ---Made By Deha#4328
